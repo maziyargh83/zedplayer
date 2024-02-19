e2dd48c5d350b4de122c7c50578663bf88af26e7
maziyargh83 <mazyar.gh83@gmail.com>
2024-02-19 03:38:48 +0330

    create download path

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -3,8 +3,18 @@ import { aria2Connection } from "lib/aria2/Aria2";
 import { aria2 } from "maria2";
 export default async function Page() {
   const v = await aria2.getVersion(aria2Connection);
-  console.log("====================================");
-  console.log(v);
-  console.log("====================================");
-  return <Button>{v.version}</Button>;
+  const addDownload = async () => {
+    "use server";
+    const res = await aria2.addUri(aria2Connection, [
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Piquet_-_Brabham-BMW_BT_54_1985-08-02.jpg/440px-Piquet_-_Brabham-BMW_BT_54_1985-08-02.jpg",
+    ]);
+    console.log("====================================");
+    console.log(res);
+    console.log("====================================");
+  };
+  return (
+    <form action={addDownload}>
+      <Button type="submit">{v.version}</Button>;
+    </form>
+  );
 }
